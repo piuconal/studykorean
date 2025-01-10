b6d5aabcafe689640082aaacc8f2516dc1ecdee7
Vũ Trọng Hiếu <85973353+piuconal@users.noreply.github.com>
2025-01-10 07:55:05 +0700

    test

---
+++ Home/home.js
@@ -0,0 +1,26 @@
+function loadContent(menu) {
+  var title = "";
+  var content = "";
+  if (menu === "Tiếng Hàn cơ bản") {
+    title = "Tiếng Hàn cơ bản"; // Tiêu đề modal khi nhấn vào Tiếng Hàn cơ bản
+    content = `
+      <ul>
+        <li><a href="#">I. Bài học 1 - 8</a></li>
+        <li><a href="#">II. Bài học 9 - 16</a></li>
+        <li><a href="#">III. Bài học 17 - 25</a></li>
+      </ul>`;
+  } else if (menu === "Tiếng Hàn cho thuyền viên") {
+    title = "Tiếng Hàn cho thuyền viên"; // Tiêu đề modal khi nhấn vào Tiếng Hàn cho thuyền viên
+    content = `
+      <ul>
+        <li><a href="#">I. Từ vựng liên quan đánh bắt</a></li>
+        <li><a href="#">II. Từ vựng sinh hoạt hàng ngày</a></li>
+        <li><a href="#">III. Các cuộc hội thoại</a></li>
+        <li><a href="#">IV. Quy tắc an toàn khi đánh bắt</a></li>
+      </ul>`;
+  }
+  // Cập nhật tiêu đề modal
+  document.getElementById("modalTitle").innerText = title;
+  // Cập nhật nội dung modal
+  document.getElementById("modalBody").innerHTML = content;
+}
