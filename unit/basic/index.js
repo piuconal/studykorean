8e56761ecf2ddb6140bb6912dd879590559e3647
Vũ Trọng Hiếu <85973353+piuconal@users.noreply.github.com>
2025-01-10 14:26:02 +0700

    fix link

--- unit/basic/index.js
+++ unit/basic/index.js
@@ -24,3 +24,15 @@
 //   // Cập nhật nội dung modal
 //   document.getElementById("modalBody").innerHTML = content;
 // }
+
+// Lắng nghe sự kiện click cho tất cả các thẻ img và span có thuộc tính data-url
+document
+  .querySelectorAll("img[data-url], span[data-url]")
+  .forEach((element) => {
+    element.addEventListener("click", function () {
+      // Lấy URL từ thuộc tính data-url
+      const url = this.getAttribute("data-url");
+      // Chuyển hướng đến URL tương ứng
+      window.location.href = url;
+    });
+  });
