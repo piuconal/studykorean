973209f91b078f471fd7ad1647679ede381fe3e9
Vũ Trọng Hiếu <85973353+piuconal@users.noreply.github.com>
2025-01-12 13:29:57 +0700

    scroll

--- unit/basic/lessons/lesson1.js
+++ unit/basic/lessons/lesson1.js
@@ -36,3 +36,30 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// Tạo nút và thêm vào DOM
+let scrollToTopBtn = document.createElement("button");
+scrollToTopBtn.id = "scrollToTopBtn";
+scrollToTopBtn.className = "btn btn-primary shadow";
+scrollToTopBtn.innerHTML = '<i class="fas fa-arrow-up"></i>';
+document.body.appendChild(scrollToTopBtn);
+
+// JavaScript để hiển thị nút khi cuộn xuống và cuộn lên đầu trang khi nhấn
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+scrollToTopBtn.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
